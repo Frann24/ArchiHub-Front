a3658aab470d872d0d99af9d270cfbe8f759e658
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-09-27 19:36:24 -0300

    agregue mas noticias

---
+++ src/appi/noticias.js
@@ -0,0 +1,137 @@
+const noticias = [
+  {
+    title: "En construcción | Well Villa",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/arquitectura-fran-silvestre-1024x683.jpg",
+    date: "29/08/2022",
+    shortDescription:
+      "Las obras del proyecto se encuentran actualmente en fase de estructura.  Ubicada en el campo de golf de PGA, en Girona...",
+    description:
+      "Las obras del proyecto se encuentran actualmente en fase de estructura.  Ubicada en el campo de golf de PGA, en Girona, la casa busca las mejores vistas del lugar en el que se encuentra. El proyecto se resuelve con dos espacios extrusionados, dos volúmenes que se superponen, en el que el superior se desplaza en dirección al lago que tiene delante dando lugar a un voladizo que busca generar una gran terraza en sombra. Dos elementos que conforman un solo proyecto, en el que se distinguen unas fachadas de gran dinamismo generadas por los huecos que se abren en ellas, aberturas que permiten una cómoda lectura de la envolvente.",
+  },
+  {
+    title: "Publicaciones | Casa del Acantilado",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/arquitectura-mediterranea-scaled.jpg",
+    date: "22/08/2022",
+    shortDescription:
+      "La Casa del Acantilado ha sido publicada en el libro Vivir en el Agua editado por Phaidon, una de las editoriales líderes mundiales en la...",
+    description:
+      "La Casa del Acantilado ha sido publicada en el libro Vivir en el Agua editado por Phaidon, una de las editoriales líderes mundiales en la publicación de libros de artes visuales, arquitectura, arte, fotografía y diseño con sede en Nueva York. El libro recopila una cuidada selección de 55 casas modernas que ofrecen una mirada accesible a la relación entre arquitectura y agua, cómo ambos elementos se armonizan, equilibran y apoyan para crear una estética verdaderamente dinámica.",
+  },
+  {
+    title: "Enjoy your summer",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/FRAN-SILVESTRE-ARQUITECTOS_PIERA-HOUSE_PHOTOS_HIGH_0011-copia-1-scaled.jpg",
+    date: "15/08/2022",
+    shortDescription:
+      "Enjoy your summer! Proyecto: Casa Piera. Fotografía: Fernando Guerra",
+    description:
+      "Enjoy your summer! Proyecto: Casa Piera. Fotografía: Fernando Guerra",
+  },
+  {
+    title: "Publicaciones | Casa del Acantilado",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/arquitectura-mediterranea-scaled.jpg",
+    date: "22/08/2022",
+    shortDescription:
+      "La Casa del Acantilado ha sido publicada en el libro Vivir en el Agua editado por Phaidon, una de las editoriales líderes mundiales en la...",
+    description:
+      "La Casa del Acantilado ha sido publicada en el libro Vivir en el Agua editado por Phaidon, una de las editoriales líderes mundiales en la publicación de libros de artes visuales, arquitectura, arte, fotografía y diseño con sede en Nueva York. </br>El libro recopila una cuidada selección de 55 casas modernas que ofrecen una mirada accesible a la relación entre arquitectura y agua, cómo ambos elementos se armonizan, equilibran y apoyan para crear una estética verdaderamente dinámica.",
+  },
+  {
+    title: "Photoshooting GANDIABLASCO | Casa del Silencio",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/gandiablasco-fran-silvestre-arquitectos-scaled.jpg",
+    date: "09/08/2022",
+    shortDescription:
+      "La marca GANDIABLASCO ha realizado un photoshooting recientemente en nuestro proyecto terminado Casa del Silencio",
+    description:
+      "La marca GANDIABLASCO ha realizado un photoshooting recientemente en nuestro proyecto terminado Casa del Silencio. Puedes conocer más imágenes de este proyecto aquí . </br>Project manager y dirección de fotografía: Alejandra Gandía-Blasco. </br>Dirección de arte: Isa Garcia y Alejandra Gandía-Blasco. </br>Fotografía: Angel Segura",
+  },
+  {
+    title: "Nuevo proyecto terminado | La casa Vacía",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/casa-vacia-arquitectos-valencia-fran-silvestre-scaled.jpg",
+    date: "03/08/2022",
+    shortDescription:
+      "El proyecto establece un diálogo entre dos tiempos; una relación entre una casa antigua y un nuevo proyecto...",
+    description:
+      "El proyecto establece un diálogo entre dos tiempos; una relación entre una casa antigua y un nuevo proyecto.</br>El objeto es ser extremadamente respetuoso con la preexistencia, conservando la parte protegida y más valiosa de la estructura original, y adaptando la casa al momento presente. Es por esto que se realiza una reposición del espacio interior, vaciándolo para regularizar los espacios y dando origen al nombre del proyecto.",
+  },
+  {
+    title: "Soul Luxury Beach Resort | The Sphinx",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/THE-SPHINX-arquitectos-valencia-fran-silvestre-scaled.jpg",
+    date: "28/07/2022",
+    shortDescription:
+      "Recientemente nuestro estudio ha sido invitado al evento del lanzamiento de Soul Luxury Beach Resort, un complejo de lujo pionero...",
+    description:
+      "Recientemente nuestro estudio ha sido invitado al evento del lanzamiento de Soul Luxury Beach Resort, un complejo de lujo pionero de 580 acres situado en Egipto, en medio de uno de los paisajes de playa natural más impresionantes del mundo en la costa norte. </br>Nuestro estudio ha participado en el masterplan diseñando la Villa de Autor con la propuesta The Sphinx, una vivienda de lujo rodeada de mansiones en la playa y casas conectadas con la naturaleza, las inigualables vistas al mar del Mediterráneo y las lagunas frente al mar, exuberantes espacios verdes y gran una variedad de comodidades únicas.",
+  },
+  {
+    estaNoticia:
+      "https://fransilvestrearquitectos.com/2022/05/02/nuevo-proyecto-terminado-n-290/",
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+];
