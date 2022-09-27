b7ac7b2fda7c54e828454401565caef148565ec4
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-09-27 20:01:47 -0300

    fix appi

---
+++ src/appi/noticias.js
@@ -0,0 +1,53 @@
+const noticias = [
+  {
+    title: "En construcción | Well Villa",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/arquitectura-fran-silvestre-1024x683.jpg",
+    date: "29/08/2022",
+    shortDescription:
+      "Las obras del proyecto se encuentran actualmente en fase de estructura.  Ubicada en el campo de golf de PGA, en Girona...",
+    description:
+      "Las obras del proyecto se encuentran actualmente en fase de estructura.  Ubicada en el campo de golf de PGA, en Girona, la casa busca las mejores vistas del lugar en el que se encuentra. El proyecto se resuelve con dos espacios extrusionados, dos volúmenes que se superponen, en el que el superior se desplaza en dirección al lago que tiene delante dando lugar a un voladizo que busca generar una gran terraza en sombra. Dos elementos que conforman un solo proyecto, en el que se distinguen unas fachadas de gran dinamismo generadas por los huecos que se abren en ellas, aberturas que permiten una cómoda lectura de la envolvente.",
+  },
+  {
+    title: "Publicaciones | Casa del Acantilado",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/arquitectura-mediterranea-scaled.jpg",
+    date: "22/08/2022",
+    shortDescription:
+      "La Casa del Acantilado ha sido publicada en el libro Vivir en el Agua editado por Phaidon, una de las editoriales líderes mundiales en la...",
+    description:
+      "La Casa del Acantilado ha sido publicada en el libro Vivir en el Agua editado por Phaidon, una de las editoriales líderes mundiales en la publicación de libros de artes visuales, arquitectura, arte, fotografía y diseño con sede en Nueva York. El libro recopila una cuidada selección de 55 casas modernas que ofrecen una mirada accesible a la relación entre arquitectura y agua, cómo ambos elementos se armonizan, equilibran y apoyan para crear una estética verdaderamente dinámica.",
+  },
+  {
+    title: "Enjoy your summer",
+    image:
+      "https://fransilvestrearquitectos.com/wp-content/uploads/2022/08/FRAN-SILVESTRE-ARQUITECTOS_PIERA-HOUSE_PHOTOS_HIGH_0011-copia-1-scaled.jpg",
+    date: "15/08/2022",
+    shortDescription:
+      "Enjoy your summer! Proyecto: Casa Piera. Fotografía: Fernando Guerra",
+    description:
+      "Enjoy your summer! Proyecto: Casa Piera. Fotografía: Fernando Guerra",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+  {
+    title: "",
+    image: "",
+    date: "",
+    shortDescription: "",
+    description: "",
+  },
+];
