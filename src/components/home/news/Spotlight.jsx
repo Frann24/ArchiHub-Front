962f0c906c900723d5b45343aaf4129754765581
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-09-28 09:01:44 -0300

    app con rutas

--- src/components/home/news/Spotlight.jsx
+++ src/components/home/news/Spotlight.jsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import news from "../../../api/news"
+// import { useParams } from "react-router-dom";
+import news from "../../../api/news";
 import NewsReel from "./NewsReel";
 
-
-
 export default function spotlight() {
+  // const params = useParams();
+  // const dispatch= useDispatch
+  const spotlight = news[0];
 
-const params = useParams()
-// const dispatch= useDispatch
-const spotlight = news[0]
+  console.log("spotlight: ", spotlight);
 
   return (
     <div>
-        <img src ={spotlight.image} width="300px" alt="news"/>
+      <div>
+        <h3>Spotlight</h3>
+        <img src={spotlight.image} width="300px" alt="news" />
+      
+
+        {/* "http://fransilvestrearquitectos.com/wp-content/uploads/2022/08/arquitectura-fran-silvestre-1024x683.jpg" */}
+
         <div>{spotlight.date}</div>
         <div>{spotlight.shortDescription}</div>
-        <NewsReel newsId={spotlight.id}/>
-
+      </div>
+      <div>
+        newsReels:
+        <NewsReel newsId={spotlight.id} />
+      </div>
     </div>
-  )
+  );
 }
-
-
