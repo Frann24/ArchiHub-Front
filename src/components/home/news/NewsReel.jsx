074c33e7b7f95093e93db6a6346f00464629e881
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-09-28 17:14:49 -0300

    posts objects

--- src/components/home/news/NewsReel.jsx
+++ src/components/home/news/NewsReel.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import news from "../../../api/news"
+import React from "react";
+import news from "../../../api/news";
 
-
-
-export default function NewsReel({newsId}) {
-
-// const params = useParams()
-// const dispatch= useDispatch
-const reel = news.filter((e)=>e.id !== newsId)
+export default function NewsReel({ newsId }) {
+  // const params = useParams()
+  // const dispatch= useDispatch
+  const reel = news.filter((e) => e.id !== newsId);
 
   return (
-    <div>
-{ reel.map((e)=>
+    <div class="grid grid-cols-4">
+      {reel.map((e) => (
         <div>
-        <img src ={e.image} width="300px" alt="news"/>
-        <div>{e.date}</div>
-        <div>{e.shortDescription}</div>
+          <img src={e.image} width="300px" alt="news" />
+          <div>{e.date}</div>
+          <div>{e.title}</div>
+          <div>{e.shortDescription}</div>
         </div>
-
-)}
+      ))}
     </div>
-  )
+  );
 }
