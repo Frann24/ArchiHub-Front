f84414147baad792a561007842f36a83c052b480
BalbianoLuciano <balbiano06@gmail.com>
2022-09-29 14:38:56 -0300

    add carrousel data

--- src/api/carouselData.js
+++ src/api/carouselData.js
@@ -2,11 +2,38 @@ const infoCarousel = [
   {
     arch: "Frank Loyd Wright",
     quotes: "“Simplicity and Repose are qualities that measure the true value of any work of art.”",
-    img: "",
-    buildingName: "Guggenheim, New York, ",
+    img: "https://cdn.getyourguide.com/img/tour/5d972ddc230c7.jpeg/146.jpg",
+    buildingName: "Solomon R. Guggenheim Museum, New York",
     description: "was the last major project designed and built by Frank Lloyd Wright between 1943 until it opened to the public in 1959, six months after his death, making it one of his longest works in creation along with one of his most popular projects."
   },
-  
+  {
+    arch: "Louis Isadore Kahn",
+    quotes: "“I sense Light as the giver of all presences, and material as spent Light. What is made by Light casts a shadow, and the shadow belongs to Light.”",
+    img: "https://arquitecturaviva.com/assets/uploads/articulos/77290/av_imagen.jpeg",
+    buildingName: "Salk Institute for Biological Studies, La Jolla",
+    description: "The original buildings of the Salk Institute were designated a historical landmark in 1991. The entire 27-acre (11 ha) site was deemed eligible by the California Historical Resources Commission in 2006 for listing in the US National Register of Historic Places."
+  },
+  {
+    arch: "Ludwig Mies Van Der Rohe",
+    quotes: "“Architecture is the will of an epoch translated into space”",
+    img: "https://www.salonemilano.it/sites/default/files/styles/libero/public/images/articles/2021-08/HEADER_1_04_NNG_210406_05661_potsdamerstr_W_Thomas_Bruns.jpg?itok=--G6jPwV",
+    buildingName: "Neue Nationalgalerie, Berlin",
+    description: "In his publication, Mies describes a seemingly floating roof plane, suspended above a single clear-span space punctuated by equidistant columns. This project is now seen as a significant move on Mies' part toward the alleviation of interior space by both defining and minimizing structural enclosure, thus joining exterior and interior space in a meaningful way."
+  },
+  {
+    arch: "Zaha Hadid",
+    quotes: "“I don’t think that architecture is only about shelter, is only about a very simple enclosure. It should be able to excite you, to calm you, to make you think.”",
+    img: "https://chttps://arquitecturaviva.com/assets/uploads/articulos/42052/av_medium__av_32781.jpeg?h=7bc46cabdn.getyourguide.com/img/tour/5d972ddc230c7.jpeg/146.jpg",
+    buildingName: "Heydar Aliyev Center, Baku",
+    description: "The Heydar Aliyev Center represents a fluid form which emerges by the folding of the landscape's natural topography and by the wrapping of individual functions of the Center. All functions of the Center, together with entrances, are represented by folds in a single continuous surface."
+  },
+  {
+    arch: "Oscar Niemeyer",
+    quotes: "“The architect's role is to fight for a better world, where he can produce an architecture that serves everyone and not just a group of privileged people.”",
+    img: "https://www.guiding-architects.net/wp-content/uploads/2019/11/1.Mondadori-building.%C2%A9Roland-Halbe.jpg",
+    buildingName: "Sede Editorial Mondadori, Milán",
+    description: "The five-story “parallel” glass tube houses offices and newsrooms. A distinctive arcade construction surrounds the glass object. Oscar's design is completed by two low, curved structures. The park that surrounds the building bears the signature of landscaper Pietro Porcinai with an artificial lake of more than 20,000 square meters."
+  }
 ]
 
 export default infoCarousel
