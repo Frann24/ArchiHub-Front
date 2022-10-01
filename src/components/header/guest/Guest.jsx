e4580af0bdbff963d22b68bcf307f72126b5b69a
Franco Ferreyra <24francoferreyra@gmail.com>
2022-10-01 18:42:54 -0300

    Header response

--- src/components/header/guest/Guest.jsx
+++ src/components/header/guest/Guest.jsx
@@ -20,12 +20,15 @@ function Guest() {
   return (
     <div>
       <ul className="xl:text-base flex gap-16 items-center">
-        <div>
+        <div className="hidden text-base
+        sm:block 
+        xl:text-lg
+        ">
           <li onClick={toggleSignIn} className="inline relative group cursor-pointer">
             <span>Sign in</span>
             <span className="absolute -bottom-1 right-0 w-0 h-0.5 bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
           </li>
-          <div className="border border-gray-900 py-1.5 inline m-2 "></div>
+          <div className="border border-gray-900 py-1 inline m-2 "></div>
           <li onClick={toggleSignUp} className="inline relative group cursor-pointer">
             <span>Register</span>
             <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
--- src/components/header/logo/Logo.jsx
+++ src/components/header/logo/Logo.jsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom'
 
 function Logo() {
   return (
-      <h2 className="py-2 xl:text-4xl font-light text-center inline">
+      <h2 /* className="py-2 xl:text-4xl font-light text-center inline" */
+      className="text-2xl font-light
+      lg:text-3xl
+      "
+      >
         <Link to="/home">
         <span className="font-semibold">ARQUI</span>HUB
         </Link>
