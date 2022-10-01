84c8324309c3568f8f598b55cefe640a7a26ea95
Jvmpers <tomaspastore200111@gmail.com>
2022-10-01 13:32:20 -0300

    implementacion actions y reducers  para post en redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // import newsSlice from "./slices/sliceNews/newsSlice";
 import news from "./slices/sliceNews/newsSlice";
 import newsSlice from "./slices/sliceNews/newsSlice"
-// import Slice1Reducer from './slices/slices1'
 import  postReducer  from "./slices/post/postSlice";
 import headerReducer from "./slices/header/headerSlice"
 
 export const store = configureStore({
   reducer:{
     post: postReducer,
-    // slice1: Slice1Reducer,
     newsSlice: newsSlice,
     header: headerReducer,
   },
