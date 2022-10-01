0c4e25b66e655dec1ba82de904898d04cc8bf1d4
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-10-01 10:37:53 -0300

    estilos news y posts

--- src/components/home/news/NewsReel.jsx
+++ src/components/home/news/NewsReel.jsx
@@ -6,7 +6,6 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { getNews1 } from "../../../redux/slices/sliceNews/newsActions";
 
-
 export default function NewsReel() {
   const dispatch = useDispatch();
   const newNews = useSelector((state) => state.newsSlice);
@@ -17,27 +16,32 @@ export default function NewsReel() {
 
   const [page, setPage] = useState(1);
   const indexLastCard = 3 * page;
-  const currentCards = newNews.news.slice(0, indexLastCard);
+  const newsPaginado = newNews.news.slice(0, indexLastCard);
   function paginado() {
     setPage(page + 1);
   }
-
+  console.log("newsPaginado: ", newsPaginado);
   return (
     <div>
-      <div className="grid grid-cols-4 ml-16 mr-16">
-        {newNews.news.map((e) => (
-          
+      <div className="grid grid-cols-3 ml-16  container mx-auto">
+        {newsPaginado.map((e) => (
           <Link to={`/newsDetail/${e.id}`}>
-            <div>
-              <img src={e.image} width="600px" alt="news" />
-              <div>{e.date}</div>
-              <p className="font-[5100]">{e.title}</p>
-              <div className="font-weight:200">{e.shortDescription}</div>
+            <div className="pr-8">
+              {e.id === 2 ? (
+                <img src="https://res.cloudinary.com/do3dbemlj/image/upload/v1664405309/news/Screen_Shot_2022-09-28_at_19.44.45_zocf1r.png" />
+              ) : (
+                <img src={e.image} width="600px" alt="news" />
+              )}
+              <div class="text-gray-400">{e.date}</div>
+              <p className="font-normal">{e.title}</p>
+              <div className="font-light">{e.shortDescription}</div>
             </div>
           </Link>
         ))}
       </div>
-      <button onClick={(e) => paginado(e)}>See more...</button>
+      <div class="ml-16 text-xl" onClick={(e) => paginado(e)}>
+        See more...
+      </div>
     </div>
   );
 }
--- src/components/home/posts/PostsReel.jsx
+++ src/components/home/posts/PostsReel.jsx
@@ -1,39 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import posts from "../../../api/posts";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function PostsReel() {
-const [page, setPage] = useState(1)
-const indexLastCard = 3 * page
-const currentCards = posts.slice(0, indexLastCard)
+  const [page, setPage] = useState(1);
+  const indexLastCard = 3 * page;
+  const currentCards = posts.slice(0, indexLastCard);
 
-
-
-function paginado () {
-  setPage(page+1)
-}
+  function paginado() {
+    setPage(page + 1);
+  }
 
   return (
     <div>
-      <h4>Posts</h4>
-        {
-        currentCards.map((post) => {
-            return (
+      <h4 class="ml-16 ">Posts</h4>
+      <div className="grid grid-cols-3 ml-16  container mx-auto">
+        {currentCards.map((post) => {
+          return (
             <div>
-                <img width='200px' height='200px' src={post.images[0]} alt='foto'/>
-                <Link to={`/postDetail/${post.id}`}>
-                <h4>{post.title}</h4>
-                </Link>
-                <p>{post.shortDescription}</p>
+              <img width="600px" src={post.images[0]} alt="foto" />
+              <Link to={`/postDetail/${post.id}`}>
+                <h4 className="font-bold ">{post.title}</h4>
+              </Link>
+              <p className="font-light">{post.shortDescription}</p>
             </div>
-            )
-            }
-        )
-        }
-      <button onClick={(e) => paginado(e)} >See more...</button>  
+          );
+        })}
+      </div>
+      <div class="ml-16 text-xl" onClick={(e) => paginado(e)}>
+        See more...
+      </div>
     </div>
-  )
-  
+  );
 }
 
-export default PostsReel
+export default PostsReel;
