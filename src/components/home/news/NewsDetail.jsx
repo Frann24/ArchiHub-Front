26f7d449c7acc94e2aefadb7cce6d535b2d47255
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-10-01 19:59:19 -0300

    arreglé el link en NewsDetail

--- src/components/home/news/NewsDetail.jsx
+++ src/components/home/news/NewsDetail.jsx
@@ -8,12 +8,18 @@ import { Link } from "react-router-dom";
 export default function NewsDetail(props) {
   const newNews = useSelector((state) => state.newsSlice.news);
   const params = useParams();
-  const detail = newNews.length > 0 && newNews.filter((e) => e.id == params.id);
   const dispatch = useDispatch();
-
+  
   useEffect(() => {
     dispatch(getNews1());
   }, [dispatch]);
+  
+  const detail = newNews.length > 0 && newNews.filter((e) => e.id == params.id);
+
+  function handleLink() {
+    // window.location.href = detail[0].url
+    window.open(detail[0].url, "_blank");
+  }
 
   console.log("params: ", params.id);
   console.log("typeof params: ", typeof params.id);
@@ -24,14 +30,19 @@ export default function NewsDetail(props) {
     <div>
       <div>
         <div className="container mx-40 mt-40">
-          <img src={detail[0].image} width="600px" alt="news" />
-          <div className="text-gray-400 mt-6">{detail[0].date}</div>
+          <img src={detail.length > 0 && detail[0].image} width="600px" alt="news" />
+          <div className="text-gray-400 mt-6">{detail.length > 0 && detail[0].date}</div>
           <p className="font-semibold text-transform: uppercase">
-            {detail[0].title}
+            {detail.length > 0 && detail[0].title}
           </p>
-          <div className="font-light max-w-prose">{detail[0].description}</div>
+          <div className="font-light max-w-prose">{detail.length > 0 && detail[0].description}</div>
 
-          <div className="font-light max-w-prose">{detail[0].url}</div>
+          <div
+            onClick={handleLink}
+            className="font-light max-w-prose underline decoration-solid text-sky-600 color: rgb(3 105 161); mb-40"
+          >
+            read original post
+          </div>
         </div>
       </div>
     </div>
