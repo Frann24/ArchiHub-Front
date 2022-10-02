e9695a449335322c11b8857b4ffe849ff16463b2
Franco Ferreyra <24francoferreyra@gmail.com>
2022-10-02 13:25:25 -0300

    Estilos header, searchbar y menu

--- src/components/header/menu/search/Search.jsx
+++ src/components/header/menu/search/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
   const [inputSearch, setInputSearch] = useState("");
@@ -14,29 +14,45 @@ function Search() {
   };
 
   const clearInputSearch = (e) => {
-    e.preventDefault()
-    setInputSearch("")
-  }
+    e.preventDefault();
+    setInputSearch("");
+  };
 
   return (
-    <form>
-      <div className="py-8 my-2 text-center">
-        <input
-          className={`${inputSearch ? 'w-1/2':'w-1/4'} bg-white shadow-md outline-none border-gray-700 p-4 focus:outline-none transition-all duration-300 ease-in  focus:w-1/2 text-base text-gray-700 pl-4 pr-20"`}
-          type="text"
-          placeholder="Search..."
-          value={inputSearch}
-          onChange={handleChange}
-        />
-          <span title="Search clean" className="-ml-20 text-gray-300 cursor-pointer">
-            {inputSearch && <FontAwesomeIcon onClick={clearInputSearch} icon={faXmark}/> }
+    <form className="">
+      <div className="m-4 flex justify-center text-sm
+      md:mx-8
+      lg:mx-16 lg:text-base
+      xl:mx-32 xl:text-lg
+      ">
+        <div className="flex w-3/4 justify-between mr-4 py-2 border border-gray-100 shadow-lg ">
+          <div className="w-11/12">
+            <FontAwesomeIcon
+              className="px-2 text-base text-gray-400 lg:px-4"
+              icon={faMagnifyingGlass}
+            />
+            <input
+              className=" w-4/5 outline-none
+              sm:w-11/12
+              "
+              type="text"
+              placeholder="Search..."
+              value={inputSearch}
+              onChange={handleChange}
+            />
+          </div>
+          <span
+            title="Search clean"
+            className=" px-2 text-gray-300 cursor-pointer"
+          >
+            {inputSearch && (
+              <FontAwesomeIcon onClick={clearInputSearch} icon={faXmark} />
+            )}
           </span>
-        <button
-          className="bg-transparent transition-all duration-200 ease-in cursor-pointer bg-gray-500 text-gray-50 font-medium shadow-md py-2 px-8 hover:bg-gray-400 ml-4 active:scale-105"
-          onClick={handleSubmit}
-        >
-          Search
-        </button>
+        </div>
+        <div className="w-24 bg-gray-700 text-center text-gray-50 cursor-pointer shadow-lg hover:bg-gray-600">
+          <button className="p-2">Search</button>
+        </div>
       </div>
     </form>
   );
