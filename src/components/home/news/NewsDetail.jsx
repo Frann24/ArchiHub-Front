138ede26ffef42688ca951fbfa18c79e0a7adfe4
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-10-01 21:53:06 -0300

    cambio minimo

--- src/components/home/news/NewsDetail.jsx
+++ src/components/home/news/NewsDetail.jsx
@@ -3,17 +3,16 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getNews1 } from "../../../redux/slices/sliceNews/newsActions";
-import { Link } from "react-router-dom";
 
 export default function NewsDetail(props) {
   const newNews = useSelector((state) => state.newsSlice.news);
   const params = useParams();
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(getNews1());
   }, [dispatch]);
-  
+
   const detail = newNews.length > 0 && newNews.filter((e) => e.id == params.id);
 
   function handleLink() {
@@ -30,12 +29,20 @@ export default function NewsDetail(props) {
     <div>
       <div>
         <div className="container mx-40 mt-40">
-          <img src={detail.length > 0 && detail[0].image} width="600px" alt="news" />
-          <div className="text-gray-400 mt-6">{detail.length > 0 && detail[0].date}</div>
+          <img
+            src={detail.length > 0 && detail[0].image}
+            width="600px"
+            alt="news"
+          />
+          <div className="text-gray-400 mt-6">
+            {detail.length > 0 && detail[0].date}
+          </div>
           <p className="font-semibold text-transform: uppercase">
             {detail.length > 0 && detail[0].title}
           </p>
-          <div className="font-light max-w-prose">{detail.length > 0 && detail[0].description}</div>
+          <div className="font-light max-w-prose">
+            {detail.length > 0 && detail[0].description}
+          </div>
 
           <div
             onClick={handleLink}
