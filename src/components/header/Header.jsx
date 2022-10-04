518ba86645d6411173f5381634cdd643964c33d6
ivoAl <ivoalegre9@gmail.com>
2022-10-04 17:54:54 -0300

    agregue /admin para ver el dashboard admin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,6 +10,7 @@ import Home from "../home/Home";
 import PostDetail from "../home/posts/PostDetail";
 import CreatePost from "../createPost/CreatePost";
 import NewsDetail from "../home/news/NewsDetail";
+import DashBoardAdmin from "../dashBoardAdmin/DashBoardAdmin";
 
 function Header() {
   const { pathname } = useLocation();
@@ -40,6 +41,7 @@ function Header() {
         <Route path="newsDetail/:id" element={<NewsDetail />} />
         <Route path="postDetail/:id" element={<PostDetail />} />
         <Route path="createpost" element={<CreatePost />} />
+        <Route path="/admin" element={<DashBoardAdmin />} />
       </Routes>
     </div>
   );
