4239eeb85dd4c5071370aa4bd65f358276d72ab6
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-10-07 14:21:39 -0300

    cambios dashAdmin

--- src/components/dashBoardAdmin/DashBoardAdmin.jsx
+++ src/components/dashBoardAdmin/DashBoardAdmin.jsx
@@ -4,55 +4,55 @@ import { getAllUsers } from "../../redux/slices/user/userActions";
 import { updateUser } from "../../redux/slices/user/userActions";
 
 function DashBoardAdmin() {
-const dispatch = useDispatch()
-const allUsers = useSelector(state => state.user.allUsers)
+  const dispatch = useDispatch();
+  const allUsers = useSelector((state) => state.user.allUsers);
 
-useEffect(() => {
-    dispatch(getAllUsers())
-}, [dispatch])   
+  useEffect(() => {
+    dispatch(getAllUsers());
+  }, [dispatch]);
 
-console.log(allUsers)
-function handleChangeStatus (id, e) {
-    dispatch(updateUser(id, {status: e.target.value}))
-}
+  console.log(allUsers);
+  function handleChangeStatus(id, e) {
+    dispatch(updateUser(id, { status: e.target.value }));
+  }
   return (
     <div>
-        <div className="flex flex-row gap-6">
-            <h2>Members</h2>
-            <h2>Mail</h2>
-            <h2>Posts</h2>
-            <h2>Proyects</h2>
-            <h2>Type</h2>
-        </div>
-        {
-            allUsers.length && allUsers.map((el) => {
-                const statusUser = el.status
-                const statusOptions = []
-                statusUser === 'active' && statusOptions.push('banned', 'inactive');
-                statusUser === 'banned' && statusOptions.push('active', 'inactive');
-                statusUser === 'inactive' && statusOptions.push('banned', 'active');
+      <div className="flex flex-row gap-6">
+        <h2>Members</h2>
+        <h2>Mail</h2>
+        <h2>Posts</h2>
+        <h2>Proyects</h2>
+        <h2>Type</h2>
+      </div>
+      {allUsers.length &&
+        allUsers.map((el) => {
+          const statusUser = el.status;
+          const statusOptions = [];
+          statusUser === "active" && statusOptions.push("banned", "inactive");
+          statusUser === "banned" && statusOptions.push("active", "inactive");
+          statusUser === "inactive" && statusOptions.push("banned", "active");
 
-                return (
-                    <div>
-                    <div className="flex flex-row gap-6">
-                        {/* <h1>{el._id}</h1> */}
-                        <h2>{el.name}</h2>
-                        <h2>{el.email}</h2>
-                        <h2>{el.posts.length}</h2>
-                        <h2>{el.projects.length === 0 ? 0 : el.projects.length}</h2>
-                        <h2>{el.type}</h2>
-                      
-                        <select onChange={(e) => handleChangeStatus(el._id, e)}>
-                            <option disabled selected defaultValue>{statusUser}</option>
-                            <option value={statusOptions[0]}>{statusOptions[0]}</option>
-                            <option value={statusOptions[1]}>{statusOptions[1]}</option>
-                        </select>
-                    </div>
-                    </div>
-                )
-            })
-        
-        }
+          return (
+            <div>
+              <div className="flex flex-row mt-6 gap-6">
+                {/* <h1>{el._id}</h1> */}
+                <h2>{el.name}</h2>
+                <h2>{el.email}</h2>
+                <h2>{el.posts.length}</h2>
+                <h2>{el.projects.length === 0 ? 0 : el.projects.length}</h2>
+                <h2>{el.type}</h2>
+
+                <select onChange={(e) => handleChangeStatus(el._id, e)}>
+                  <option disabled selected defaultValue>
+                    {statusUser}
+                  </option>
+                  <option value={statusOptions[0]}>{statusOptions[0]}</option>
+                  <option value={statusOptions[1]}>{statusOptions[1]}</option>
+                </select>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
