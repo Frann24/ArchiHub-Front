9c0e2844b72f5b1350bd0f0bdd11ebeaa652a7ff
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-10-10 22:22:16 -0300

    profile

--- src/components/dashBoardUser/DashBoardUser.jsx
+++ src/components/dashBoardUser/DashBoardUser.jsx
@@ -9,20 +9,21 @@ import Favourites from "./Favourites";
 import Reviews from "./Reviews";
 import { getAllReviews } from "../../redux/slices/review/reviewActions";
 import { useParams } from "react-router-dom";
+import Profile from "./Profile";
 
 export default function DashBoardUser() {
-  const dispatch = useDispatch()
-  const { value } = useParams()
-  const userLogeado = JSON.parse(localStorage.getItem("token"))
-  const user = useSelector(state => state.user.user)
-  const [state, setState] = useState(value)
-  const [profile, setProfile] = useState(false)
+  const dispatch = useDispatch();
+  const { value } = useParams();
+  const userLogeado = JSON.parse(localStorage.getItem("token"));
+  const user = useSelector((state) => state.user.user);
+  const [state, setState] = useState(value);
+  const [profile, setProfile] = useState(false);
 
   useEffect(() => {
-    dispatch(getUser(userLogeado.userId))
-    dispatch(getAllReviews())
-  }, [dispatch])
- 
+    dispatch(getUser(userLogeado.userId));
+    dispatch(getAllReviews());
+  }, [dispatch]);
+
   function handleChange(e) {
     setState(e.target.value);
   }
@@ -32,7 +33,89 @@ export default function DashBoardUser() {
 
   return (
     <div className="w-1/2 mx-auto mt-6">
-      <div className="flex flex-wrap mb-12 w-full">
+      {profile ? (
+        <div className="">
+          <FormEditProfile id={userLogeado.userId} user={user} />
+        </div>
+      ) : (
+        <div className="">
+          <div>
+          <Profile
+            state={state}
+            user={user}
+            handleChange={handleChange}
+            handleEditProfile={handleEditProfile}
+            // id={{ id: userLogeado.userId }}
+            id={userLogeado.userId}
+          />
+          </div>
+          <div className="mt-20">
+            {/* <button
+            onClick={(e) => handleEditProfile(e)}
+            className="bg-slate-300 cursor-pointer w-32 h-8"
+          >
+            Edit Profile
+          </button> */}
+          </div>
+          <div>
+            <div>
+              <div className="flex flex-row gap-10 mt-16 ml-12 w-1/2">
+                <div className="tracking-wider text-lg hover:border-b-2 border-slate-300">
+                  <button value="projects" onClick={(e) => handleChange(e)}>
+                    Projects
+                  </button>
+                </div>
+
+                <div className="tracking-wider text-lg hover:border-b-2 border-slate-300">
+                  <button value="posts" onClick={(e) => handleChange(e)}>
+                    Posts
+                  </button>
+                </div>
+
+                <div className="tracking-wider text-lg border-b-2 border-white hover:border-b-2 hover:border-slate-300">
+                  <button value="reviews" onClick={(e) => handleChange(e)}>
+                    Reviews
+                  </button>
+                </div>
+
+                <div className="tracking-wider text-lg border-b-2 border-white hover:border-b-2 hover:border-slate-300">
+                  <button value="favourites" onClick={(e) => handleChange(e)}>
+                    Favourites
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              {state === "projects" && (
+                <div>
+                  <Projects id={userLogeado.userId} />
+                </div>
+              )}
+              {state === "posts" && (
+                <div>
+                  <Post
+                    id={userLogeado.userId}
+                    key={userLogeado.userId}
+                    user={user}
+                  />
+                </div>
+              )}
+              {state === "reviews" && (
+                <div>
+                  <Reviews />
+                </div>
+              )}
+              {state === "favourites" && (
+                <div>
+                  <Favourites />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* <div className="flex flex-wrap mb-12 w-full">
         <div className="mt-6 ml-12 grid grid-cols-1 sm:grid-cols-2 sm:gap-12 md:gap-20 ">
           <img
             src={`${user.avatar}`}
@@ -136,7 +219,7 @@ export default function DashBoardUser() {
             <Favourites />
           </div>
         )}
-      </div>
+      </div> */}
     </div>
   );
 }
