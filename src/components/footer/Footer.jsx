e411a1d5114088c5252212afe2adec8cb4e33320
Franco Ferreyra <24francoferreyra@gmail.com>
2022-10-15 15:17:52 -0300

    estilos forgot password, resetpassword y acomode el footer para que siempre este abajo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,10 +3,9 @@ import Logo from '../header/logo/Logo'
 
 function Footer() {
   return (
-    <div className='w-full text-white bg-gray-900 bottom-0'>
+    <footer className='w-full text-white bg-gray-900'>
       <div className='text-gray-50 p-4 text-center text-3xl'><Logo/></div>
-
-    </div>
+    </footer>
   )
 }
 
