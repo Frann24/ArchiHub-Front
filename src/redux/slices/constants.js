ceb81275bd522b98055d6f0b8c3effcfb148226a
BalbianoLuciano <balbiano06@gmail.com>
2022-10-18 11:51:07 -0300

    change constants

--- src/redux/slices/constants.js
+++ src/redux/slices/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001/"
+export const BASE_URL = "https://backend-arquihub-production.up.railway.app/"
 /* https://backend-arquihub-production.up.railway.app */
 export const USERS = `${BASE_URL}api/user`
 
