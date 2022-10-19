f1a2a6772f652565e5f11395f354c223ab778ac6
Gonzalo <102692082+gnz6@users.noreply.github.com>
2022-10-19 20:09:46 -0300

    404con estilazos

--- src/components/errors/NotFound.jsx
+++ src/components/errors/NotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import "./NotFound.css"
+import notFoundImg from "../../assets/notFound/notFoundImg.jpeg"
 
 function NotFound () {
     const navigate = useNavigate()
@@ -11,7 +12,7 @@ function NotFound () {
   return (
     <div className='errorDiv'>
         <h1 className='errorTitle'>Sorry, we can´t find the page you are looking for</h1>
-        <img className='errorImg' src='https://www.kindpng.com/picc/m/46-463914_under-construction-png-transparent-png.png' alt="site"/>
+        <img className='errorImg' src={notFoundImg} alt="site"/>
         <button className='errorBtn' onClick={handleReturn}>
         Return to Homepage ⮌
         </button>
